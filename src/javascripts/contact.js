845b8aa75f37c1a10e35f1e8d90044bd3ac1aff6
RIPsteRXX <ethanmeier113@gmail.com>
2022-12-09 10:36:35 -0700

    one page left

---
+++ src/javascripts/contact.js
@@ -0,0 +1,38 @@
+import _default from "@popperjs/core/lib/modifiers/popperOffsets"
+import "bootstrap"
+import { Alert } from "bootstrap"
+
+
+function send(event){
+ 
+    event.preventDefault()
+    
+    let email = document.getElementById("email").value
+    let body = document.getElementById("body").value
+
+    if (email != "" && body != ""){
+        if (confirm("Are you sure you want to send this email?")){
+            document.getElementById("email").value = ""
+            document.getElementById("subject").value = ""
+            document.getElementById("body").value = ""
+            document.getElementById("file").value = null
+            alert("Email Sent!");
+        }
+    }
+    else {
+        alert("No email/body entered")
+    }
+}
+
+
+document.getElementById('send').onclick = send
+document.getElementById('cancel').onclick = function(){
+    if (confirm ("Are you sure you wish to cancel and discard?")){
+ 
+        document.getElementById("email").value = ""
+        document.getElementById("subject").value = ""
+        document.getElementById("body").value = ""
+        document.getElementById("file").value = null
+        }
+}
+
